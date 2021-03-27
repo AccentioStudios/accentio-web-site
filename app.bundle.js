d64a758fbcf3cb6915a30ee59987cccd6dadf35b
Alvaro Gabriel Gomez <alvarogabrielgomez@gmail.com>
2021-03-27 19:04:45 -0300

    limpiando archivos y quitando dependencias sini usar

--- app.bundle.js
+++ app.bundle.js
@@ -1,1 +1,1 @@
-(self.webpackChunkaccentio_web=self.webpackChunkaccentio_web||[]).push([[143],{562:(e,c,s)=>{"use strict";s(486),s(669)}},0,[[562,666,216]]]);
+(self.webpackChunkaccentio_web=self.webpackChunkaccentio_web||[]).push([[143],{562:()=>{}},0,[[562,666]]]);
